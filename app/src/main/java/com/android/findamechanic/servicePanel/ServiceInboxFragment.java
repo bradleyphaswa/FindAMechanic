2f215407709a219999ffac4430da78b548d43b47
Bradley <bradleyphaswana@gmail.com>
2022-12-26 00:40:07 +0200

    Part 16 in progress. Logout menu completed.

---
+++ app/src/main/java/com/android/findamechanic/servicePanel/ServiceInboxFragment.java
@@ -0,0 +1,23 @@
+package com.android.findamechanic.servicePanel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.android.findamechanic.R;
+
+public class ServiceInboxFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_service_inbox, null);
+        getActivity().setTitle("Inbox");
+        return v;
+    }
+}
